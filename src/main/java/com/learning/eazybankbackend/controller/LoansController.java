dd97900a702dca21a6e2c69372e069c94bd2f8c9
Himanshu Pandey <himanshupandey2411@gmail.com>
2023-03-12 19:21:09 +0530

    added method level security

--- src/main/java/com/learning/eazybankbackend/controller/LoansController.java
+++ src/main/java/com/learning/eazybankbackend/controller/LoansController.java
@@ -2,6 +2,7 @@ package com.learning.eazybankbackend.controller;
 
 import com.learning.eazybankbackend.model.Loans;
 import com.learning.eazybankbackend.repository.LoanRepository;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class LoansController {
     }
 
     @GetMapping("/myLoans")
+    @PostAuthorize("hasRole('USER')")
     public List<Loans> getLoanDetails(@RequestParam Long id){
         Optional<List<Loans>> loans = loanRepository.findByCustomerIdOrderByStartDateDesc(id);
         return loans.orElse(null);
--- src/main/java/com/learning/eazybankbackend/repository/LoanRepository.java
+++ src/main/java/com/learning/eazybankbackend/repository/LoanRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface LoanRepository extends JpaRepository<Loans, Long> {
-    @PreAuthorize("hasRole('ROOT')")
+    @PreAuthorize("hasRole('USER')")
+
     Optional<List<Loans>> findByCustomerIdOrderByStartDateDesc(Long customerId);
 }
