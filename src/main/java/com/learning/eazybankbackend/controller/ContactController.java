a03aef7298b3447cf6270ad4a98ee8cdaed27f6d
Himanshu Pandey <himanshupandey2411@gmail.com>
2023-03-14 13:32:09 +0530

    added pre and post filter

--- src/main/java/com/learning/eazybankbackend/controller/ContactController.java
+++ src/main/java/com/learning/eazybankbackend/controller/ContactController.java
@@ -2,11 +2,14 @@ package com.learning.eazybankbackend.controller;
 
 import com.learning.eazybankbackend.model.Contact;
 import com.learning.eazybankbackend.repository.ContactRepository;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Random;
 
 @RestController
@@ -19,9 +22,13 @@ public class ContactController {
     }
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact){
+    @PreFilter("filterObject.contactName != Test")
+    @PostFilter("filterObject.contactName != Test")
+   /*authorization criteria on the input only if it is of type collection interface.*/
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts){
+        Contact contact = contacts.get(0);
         contact.setContactId(getServiceRequestNumber());
-        return contactRepository.save(contact);
+        return List.of(contactRepository.save(contact));
     }
 
     public String getServiceRequestNumber(){
